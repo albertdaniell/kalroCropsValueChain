567e3a9dc3f6aad43901ea1ef565c91107b330b9
albertdaniell <albertagoya@gmail.com>
2020-09-29 15:40:19 +0300

    first commit

--- src/layouts/ErrorPage/ErrorPage.js
+++ src/layouts/ErrorPage/ErrorPage.js
@@ -3,6 +3,7 @@ import AppLogo from '../AppLogo/AppLogo'
 import AppNav from '../AppNav/AppNav'
 import Headerimage from '../HeaderImage/Headerimage';
 import Preheader from '../PreHeader/Preheader'
+import image1 from '../../images/404.png'
 
 function ErrorPage() {
     const [appName,setAppName]=useState('Go to Home');
@@ -11,7 +12,7 @@ function ErrorPage() {
              <Preheader></Preheader>
             <AppLogo appName={appName} to='/'></AppLogo>
             <AppNav appName={appName}></AppNav>
-            <Headerimage pageHeader="404 Error! Page Not Found"></Headerimage>
+            <Headerimage img={image1} pageHeader="404 Error! Page Not Found"></Headerimage>
             
         </div>
     )
