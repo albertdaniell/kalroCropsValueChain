52d618bda953b30b67e673fb2977224f57773560
albertdaniell <albertagoya@gmail.com>
2020-09-29 14:35:25 +0300

    first commit

--- src/layouts/HeaderImage/Headerimage.js
+++ src/layouts/HeaderImage/Headerimage.js
@@ -6,7 +6,7 @@ function Headerimage(props) {
         <div id="headerImg" style={{backgroundImage:`url(${img})`,backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
             <div style={{backgroundColor:'rgba(173, 153, 87, 0.16)',color:'white',position:'absolute',height:250,width:'100%'}}>
               
-                <div id="headerHeading" >
+                <div id="headerHeading" className="animate__animated animate__fadeInDown">
                     {pageHeader}
                 </div>
             </div>
