3a3e0814ed5e5088e615e71ec8a6af79c5aeac59
albertdaniell <albertagoya@gmail.com>
2020-09-29 15:27:16 +0300

    first commit

--- src/layouts/ErrorPage/ErrorPage.js
+++ src/layouts/ErrorPage/ErrorPage.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AppLogo from '../AppLogo/AppLogo'
+import AppNav from '../AppNav/AppNav'
+import Headerimage from '../HeaderImage/Headerimage';
+import Preheader from '../PreHeader/Preheader'
 
 function ErrorPage() {
+    const [appName,setAppName]=useState('Go to Home');
     return (
         <div>
-            <h2>404 Error! Page Not Found</h2>
+             <Preheader></Preheader>
+            <AppLogo appName={appName} to='/'></AppLogo>
+            <AppNav appName={appName}></AppNav>
+            <Headerimage pageHeader="404 Error! Page Not Found"></Headerimage>
+            
         </div>
     )
 }
