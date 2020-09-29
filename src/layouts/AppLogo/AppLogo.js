062c236ef0f26c69c5649e8d33a41002087e4830
albertdaniell <albertagoya@gmail.com>
2020-09-29 14:46:59 +0300

    first commit

--- src/layouts/AppLogo/AppLogo.js
+++ src/layouts/AppLogo/AppLogo.js
@@ -3,7 +3,7 @@ import React from 'react'
 function AppLogo(props) {
     return (
         <div id="appLogoDiv" style={{}}>
-            <a id="appLogo" style={{}} href="/">{props.appName}</a>
+            <a id="appLogo" style={{}} href="/beansLanding">{props.appName}</a>
         </div>
     )
 }
