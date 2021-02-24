53a44703f1fc703bccac6e954cb9574364feb0b4
Albert Daniel <albertagoya@gmail.com>
2021-02-24 15:12:41 +0300

    Adding images

--- src/images/OrgLogos.js
+++ src/images/OrgLogos.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import image1 from '../../images/kalrologo90-64-2.gif'
 import image2 from '../../images/laikipia_emblem.png'
-import image3 from '../../images//ACWICT.png'
-
+import image3 from '../../images/ACWICT.png'
 
 function OrgLogos() {
     return (
         <div id='appLogos'>
-       <a href='https://kalro-cropslivestock.web.app/'>
+          <a href='https://kalro-cropslivestock.web.app/'>
           <img src={image2} id='appLogoimg2'></img>
           <img src={image1}></img>
           <img src={image3}></img>
-
             
           </a>
-           
         </div>
     )
 }
